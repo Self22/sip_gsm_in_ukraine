6f56bc3793b15d2bd058bd60cfc368cd274e519b
Evgeniy Sayenko <evgeniy.sayenko@gmail.com>
2016-08-31 00:58:39 +0300

    Начаты номера

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -1,42 +1,85 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
     //аккордеон
-    (function(){
+    (function () {
 
         var flag = true;
 
-        $('.channel-acco__trigger-link').on('click', function (e){
+        $('.channel-acco__trigger-link').on('click', function (e) {
             e.preventDefault();
 
             var
                 $this = $(this),
                 container = $this.closest('.channels__list'),
                 item = $this.closest('.channel__item'),
-                currentContent =item.find('.channel-acco__descr'),
+                currentContent = item.find('.channel-acco__descr'),
                 duration = 500;
 
             if (flag) {
                 flag = false;
 
 
-            if(!item.hasClass('active')) {
-                item.addClass('active')
-                    .siblings()
-                    .removeClass('active')
-                    .find('.channel-acco__descr')
-                    .slideUp(duration);
+                if (!item.hasClass('active')) {
+                    item.addClass('active')
+                        .siblings()
+                        .removeClass('active')
+                        .find('.channel-acco__descr')
+                        .slideUp(duration);
 
                     currentContent.slideDown(duration, function () {
                         flag = true
                     });
-            }
-            else{
-                item.removeClass('active');
-                currentContent.slideUp(function() {
-                    flag = true
-                });
-            }
+                }
+                else {
+                    item.removeClass('active');
+                    currentContent.slideUp(function () {
+                        flag = true
+                    });
+                }
 
-        }
+            }
         })
     })();
+
+
+});
+
+$(document).ready(function() {
+
+    var flag = true;
+
+    $('.numbers__tab-link').on('click', function (e) {
+        e.preventDefault();
+
+        var
+            $this = $(this),
+            item = $this.closest('.numbers__tab'),
+            container = $this.closest('.tabs'),
+            content = container.find('.numbers__content-item'),
+            ndx = item.index(),
+            reqItem = content.eq(ndx),
+            activeItem = content.filter('.numbers__content-item-active');
+
+        if (flag) {
+            flag = false;
+
+
+            item.addClass('numbers__tab-active')
+                .siblings()
+                .removeClass('numbers__tab-active');
+
+            activeItem.fadeOut(500, function () {
+                reqItem.fadeIn(500, function () {
+                    $(this).addClass('numbers__content-item-active')
+                        .siblings()
+                        .removeClass('numbers__content-item-active');
+                    flag = true
+                });
+            });
+
+        }
+
+
+    })
+
 });
--- prod/js/main.js
+++ prod/js/main.js
@@ -1,2 +1,26 @@
-$(document).ready(function(){!function(){var n=!0;$(".channel-acco__trigger-link").on("click",function(c){c.preventDefault();var e=$(this),s=(e.closest(".channels__list"),e.closest(".channel__item")),a=s.find(".channel-acco__descr"),i=500;n&&(n=!1,s.hasClass("active")?(s.removeClass("active"),a.slideUp(function(){n=!0})):(s.addClass("active").siblings().removeClass("active").find(".channel-acco__descr").slideUp(i),a.slideDown(i,function(){n=!0})))})}()});
+$(document).ready(function () {
+    !function () {
+        var e = !0;
+        $(".channel-acco__trigger-link").on("click", function (n) {
+            n.preventDefault();
+            var t = $(this), s = (t.closest(".channels__list"), t.closest(".channel__item")), i = s.find(".channel-acco__descr"), c = 500;
+            e && (e = !1, s.hasClass("active") ? (s.removeClass("active"), i.slideUp(function () {
+                e = !0
+            })) : (s.addClass("active").siblings().removeClass("active").find(".channel-acco__descr").slideUp(c), i.slideDown(c, function () {
+                e = !0
+            })))
+        })
+    }()
+}), $(document).ready(function () {
+    var e = !0;
+    $(".numbers__tab-link").on("click", function (n) {
+        n.preventDefault();
+        var t = $(this), s = t.closest(".numbers__tab"), i = t.closest(".tabs"), c = i.find(".numbers__content-item"), a = s.index(), l = c.eq(a), o = c.filter(".numbers__content-item-active");
+        e && (e = !1, s.addClass("numbers__tab-active").siblings().removeClass("numbers__tab-active"), o.fadeOut(500, function () {
+            l.fadeIn(500, function () {
+                $(this).addClass("numbers__content-item-active").siblings().removeClass("numbers__content-item-active"), e = !0
+            })
+        }))
+    })
+});
 //# sourceMappingURL=main.js.map
