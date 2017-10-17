f5980ef714ebc86d67d3a8969d288b0bfc1dfd45
Eugen <evgeniy.sayenko@gmail.com>
2017-10-17 22:01:51 +0300

    Глобальная переделка сайта: рус\укр сделано, настраиваю отправку и маски

--- _dev/js/main.js
+++ _dev/js/main.js
@@ -1,3 +1,10 @@
+/////////////// mask
+$(document).ready(function () {
+    $('.order__tel-input').mask('+38 (000) 000-00-00');
+});
+
+//// accordeon channels
+
 $(document).ready(function () {
 
     //���������
@@ -193,3 +200,81 @@ $('.close__popup').on('click', function (e) {
 
 
 })
+
+////////////////// validation order__form
+
+(function () {
+
+    var app = {
+
+        initialize: function () {
+            this.setUpListeners();
+        },
+
+        setUpListeners: function () {
+            $('#popupform-order').on('submit', app.submitForm);
+        },
+
+        submitForm: function (e) {
+            e.preventDefault();
+
+            var form = $(this),
+                submitBtn = form.find('button[type="submit"]');
+
+            if (app.validateForm(form) === false) return false;
+
+            submitBtn.attr('disabled', 'disabled');
+
+            console.log('Form__post!');
+            var str = form.serialize();
+
+            $.ajax({
+                url: '../contact-form/contact_process.php',
+                type: 'POST',
+                data: str
+            })
+                .done(function (msg) {
+                    if (msg === "OK") {
+                        var result = "<div = 'bg-success'>Спасибо за заявку! Мы вам перезвоним!</div>"
+                        form.html(result);
+                    } else {
+                        form.html(msg);
+                    }
+                })
+                .always(function () {
+                    submitBtn.removeAttr('disabled');
+                });
+
+        },
+
+        validateForm: function (form) {
+            var inputs = form.find('input'),
+                valid = true;
+
+
+            $.each(inputs, function (index, val) {
+                var input = $(val),
+                    val = input.val(),
+                    formGroup = input.parents('.form-group');
+
+
+                if (val.length === 0) {
+                    formGroup.addClass('has-warning').removeClass('has-success');
+                    input.addClass('form-control-warning').removeClass('form-control-success');
+                    valid = false;
+                } else {
+                    formGroup.addClass('has-success').removeClass('has-warning');
+                    input.addClass('form-control-success').removeClass('form-control-warning');
+                }
+            });
+
+            return valid;
+        },
+
+
+    }
+
+    app.initialize();
+
+}());
+
